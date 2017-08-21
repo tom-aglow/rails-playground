ddbc00e46c25ef98ed06ec3eec5400b4a0eb81ed
tom aglow <tom@ohhhh.me>
2017-08-20 21:51:34 -0700

    created simple links between pages // got parameters from URL

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,13 +3,20 @@ class DemoController < ApplicationController
   layout false
 
   def index
-    render('hello')
+    render('index')
   end
 
   def hello
+    @array = [1, 2, 3, 4, 5]
+    @id = params[:id]
+    @page = params[:page]
   end
 
   def other_hello
     redirect_to(:action => 'index')
+  end
+
+  def google
+    redirect_to('http://google.com')
   end
 end
