40e6d7c4f6a4283cf269abb04b057c1d4f6fcac5
tom aglow <tom@ohhhh.me>
2017-08-22 22:03:30 -0700

    added new and create method to SubjectsController

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -11,9 +11,22 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => 'default'})
   end
 
   def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      # If save fails, redisplay the form so user can fix issues
+      render('new')
+    end
+
   end
 
   def edit
@@ -26,5 +39,12 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
